1ab25cf893d2d467e13e557d0249b24f34d3ef63
Umairulislam <umairulislam09@gmail.com>
2025-05-01 19:53:09 +0500

    clean login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -23,23 +23,6 @@ import Link from "next/link"
 import { showToast } from "@/store/toastSlice"
 import { createCookie } from "@/helpers/cookie"
 
-// // Utility function to decode a JWT token
-// function decodeJwt(token) {
-//   try {
-//     // Split the token into its three parts
-//     const [header, payload, signature] = token.split(".")
-
-//     // Decode the payload (Base64 URL decode)
-//     const decodedPayload = atob(payload.replace(/-/g, "+").replace(/_/g, "/"))
-
-//     // Parse the decoded payload as JSON
-//     return JSON.parse(decodedPayload)
-//   } catch (error) {
-//     console.error("Failed to decode JWT:", error)
-//     return null
-//   }
-// }
-
 const LoginPage = () => {
   const router = useRouter()
   const dispatch = useDispatch()
@@ -168,7 +151,6 @@ const LoginPage = () => {
             flexDirection: "column",
             gap: 3,
           }}
-          onSubmit={handleSubmit(onSubmit)}
         >
           {/* Email Field */}
           <Controller
