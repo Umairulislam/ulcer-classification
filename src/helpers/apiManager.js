925ffdc0f01aa2aa53c697d1f7554acd63722126
Umairulislam <umairulislam09@gmail.com>
2026-02-23 00:14:17 +0500

    update apiManager function

--- src/helpers/apiManager.js
+++ src/helpers/apiManager.js
@@ -1,5 +1,4 @@
 import axios from "axios"
-import { deleteCookie } from "@/helpers/cookie"
 import { getCookieOnClient } from "@/helpers/clientCookies"
 
 export const apiManager = axios.create({
@@ -11,23 +10,20 @@ export const apiManager = axios.create({
 })
 
 // Request Interceptor: Adds the token to headers
-apiManager.interceptors.request.use(
-  (config) => {
+apiManager.interceptors.request.use((config) => {
+  // Guard: only run on client
+  if (typeof window !== "undefined") {
     const token = getCookieOnClient("accessToken")
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-
-  (error) => Promise.reject(error)
-)
+    if (token) config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, Promise.reject)
 
 // Response Interceptor: Handles expired sessions
 apiManager.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 && typeof window !== "undefined") {
       // Clear cookie and force redirect if token is invalid
       document.cookie = "accessToken=; Max-Age=0; path=/;"
       window.location.href = "/login"
